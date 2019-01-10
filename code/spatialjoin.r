fc57897f4b8657f708546561b9024a91c8f3f400
Spencer Wood <woodsp@stanford.edu>
2019-01-10 13:48:35 -0800

    Beta code to cleaning and intersecting angling events with waterbody shapes, then export a cleaned table of visitors x lakes x day. Outputs go in an analysis/ directory, which isn't tracked in repo.

--- code/spatialjoin.r
+++ code/spatialjoin.r
@@ -5,8 +5,11 @@
 ## Inputs are angling events and polygons of lakes
 ## Outputs are table of angling events by lake
 
+## TODO: First version for WA.  Need to scale-up to nation.
+
 # Read angling point events
-angling_hotspots <- read.csv("/Users/rachelfricke/Desktop/Olden_Lab/Angler_Behavior/IB_Data_Mar18.csv", header = TRUE, na.strings = NULL)
+angling_hotspots_raw <- read.csv("../data/IB_Data_Mar18.csv", header = TRUE, na.strings = NULL)
+angling_hotspots <- angling_hotspots_raw
 
 # Split timestamp column into date and time, delete extraneous variables
 library(tidyr)
@@ -16,9 +19,15 @@ angling_hotspots <- angling_hotspots[,-c(5:20,23)]
 # Subset 2017 records
 angling_hotspots <- angling_hotspots[which((angling_hotspots$date > "2016-12-31") & (angling_hotspots$date < "2018-01-01")), names(angling_hotspots) %in% c("hotspot_id", "latitude", "longitude", "bobber_id", "date", "time")]
 
+# Subset USA records
+angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 45 & angling_hotspots$latitude < 50 & angling_hotspots$longitude > -127 & angling_hotspots$longitude < -116, ]
+# angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 25 & angling_hotspots$latitude < 50 & angling_hotspots$longitude > -127 & angling_hotspots$longitude < -65, ]
+
+
 # Read NHD spatial data
 library(rgdal)
-NHD_WA <- readOGR(dsn="/Users/rachelfricke/Desktop/Olden_Lab/Angler_Behavior/NHD_WA", layer="NHDWaterbody", stringsAsFactors = )
+NHD_WA_raw <- readOGR(dsn="../data/", layer="NHDWaterbody_albers", stringsAsFactors=F)
+NHD_WA <- NHD_WA_raw
 
 # Filter NHD to lakes
 NHD_WA <- NHD_WA[which((NHD_WA@data$FCode >= "39000") & (NHD_WA@data$FCode <= "46599")), names(NHD_WA@data) %in% c("OBJECTID", "Permanent_", "FDate", "Resolution", "GNIS_ID", "GNIS_Name", "AreaSqKm", "Elevation", "ReachCode", "FType", "FCode", "Visibility", "Shape_Leng", "Shape_Area")]
@@ -26,12 +35,28 @@ NHD_WA <- NHD_WA[which((NHD_WA@data$FCode >= "39000") & (NHD_WA@data$FCode <= "4
 # Buffer NHD lakes by 50m
 library(raster)
 library(rgeos)
-buffer(NHD_WA, width = 50)
+NHD_WA_buff <- gBuffer(NHD_WA, width=50, byid=T)
 
 # Intersect angling points with buffered lake polys
+#  start by reprojecting angling events to Albers to match waterbodies
+angling_hotspots_sp <- SpatialPointsDataFrame(angling_hotspots[ ,c("longitude","latitude")], angling_hotspots, proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+angling_hotspots_sp_albers <- spTransform(angling_hotspots_sp, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+# plot(NHD_WA_buff)
+# plot(angling_hotspots_sp_albers, add=T)
+angNwat <- over(angling_hotspots_sp_albers, NHD_WA_buff)
+# add lake ID onto angling events
+angling_hotspots_wat <- angling_hotspots
+angling_hotspots_wat$OBJECTID <- angNwat$OBJECTID
 
+# Identified hotspots with duplicate bobber ID, lake, and date combinations to get trips
+angling_hotspots_wat$bdoID <- paste(angling_hotspots_wat$bobber_id, angling_hotspots_wat$OBJECTID, angling_hotspots_wat$date, sep='x')
+angling_hotspots_wat$uniq <- !duplicated(angling_hotspots_wat$bdoID)
 
-# Remove hotspots with duplicate bobber ID, lake, and date combinations to get trips
+# Table for first round of analyses
+toa <- angling_hotspots_wat[angling_hotspots_wat$uniq==T,c("bobber_id","date","OBJECTID")]
+
+# Write table to analyse
+write.csv(toa, "../analysis/anglingXwater.csv", row.names=F)
 
 
 
