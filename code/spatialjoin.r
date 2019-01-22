08aaee2bb03c6134b6666760c7ce47ecb679b040
Spencer Wood <woodsp@stanford.edu>
2019-01-22 12:43:58 -0800

    Some small steps towards processing multiple states. State name is specified in the NHDWaterbody shp file name. NHDWaterbody shp files get reprojected on the fly, now. Angling events get clipped to bbox of the state that is being processed (based on the NHD polygons)
    See Issue #3.

--- code/spatialjoin.r
+++ code/spatialjoin.r
@@ -26,8 +26,6 @@ angling_hotspots <- angling_hotspots[,c("hotspot_id", "latitude", "longitude", "
 # Subset 2017 records
 angling_hotspots <- angling_hotspots[which((angling_hotspots$date > "2016-12-31") & (angling_hotspots$date < "2018-01-01")), names(angling_hotspots) %in% c("hotspot_id", "latitude", "longitude", "bobber_id", "date", "hourMT")]
 
-# Subset WA records
-angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 45 & angling_hotspots$latitude < 50 & angling_hotspots$longitude > -127 & angling_hotspots$longitude < -116, ]
 # angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 25 & angling_hotspots$latitude < 50 & angling_hotspots$longitude > -127 & angling_hotspots$longitude < -65, ]
 
 ## Import/Munge Waterbodies
@@ -35,11 +33,13 @@ angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 45 & angling_ho
 library(rgdal)
 library(raster)
 library(rgeos)
-NHD_WA_raw <- readOGR(dsn="./data/", layer="NHDWaterbody_albers", stringsAsFactors=F)
-NHD_WA <- NHD_WA_raw
+NHD_WA_raw <- readOGR(dsn="./data/", layer="NHDWaterbody_WA", stringsAsFactors=F)
 
 # Filter NHD to lakes
-NHD_WA <- NHD_WA[which((NHD_WA@data$FCode >= "39000") & (NHD_WA@data$FCode <= "46599")), names(NHD_WA@data) %in% c("OBJECTID", "Permanent_", "FDate", "Resolution", "GNIS_ID", "GNIS_Name", "AreaSqKm", "Elevation", "ReachCode", "FType", "FCode", "Visibility", "Shape_Leng", "Shape_Area")]
+NHD_WA <- NHD_WA_raw[which((NHD_WA_raw@data$FCode >= "39000") & (NHD_WA_raw@data$FCode <= "46599")), names(NHD_WA_raw@data) %in% c("OBJECTID", "Permanent_", "FDate", "Resolution", "GNIS_ID", "GNIS_Name", "AreaSqKm", "Elevation", "ReachCode", "FType", "FCode", "Visibility", "Shape_Leng", "Shape_Area")]
+
+# Reproject NHD polygons to albers
+NHD_WA <- spTransform(NHD_WA, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
 
 # Add lake centroid locations to NHD_WA
 NHD_WA_cent <- gCentroid(NHD_WA, byid=T)
@@ -50,14 +50,19 @@ NHD_WA_buff <- gBuffer(NHD_WA, width=50, byid=T)
 
 
 ## Intersect angling points with buffered lake polys
-#  start by reprojecting angling events to Albers to match waterbodies
+# Start by reprojecting angling events to Albers to match waterbodies
 angling_hotspots_sp <- SpatialPointsDataFrame(angling_hotspots[ ,c("longitude","latitude")], angling_hotspots, proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
-angling_hotspots_sp_albers <- spTransform(angling_hotspots_sp, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+angling_hotspots_sp <- spTransform(angling_hotspots_sp, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+
+# Subset angling events within bounding box of NHD AOI
+angling_hotspots_sp <- angling_hotspots_sp[angling_hotspots_sp$latitude > bbox(NHD_WA_buff)[2] & angling_hotspots_sp$latitude < bbox(NHD_WA_buff)[4] & angling_hotspots_sp$longitude > bbox(NHD_WA_buff)[1] & angling_hotspots_sp$longitude < bbox(NHD_WA_buff)[3], ]
+
 # plot(NHD_WA_buff)
-# plot(angling_hotspots_sp_albers, add=T)
-angNwat <- over(angling_hotspots_sp_albers, NHD_WA_buff)
+# plot(NHD_WA_cent)
+# plot(angling_hotspots_sp, add=T)
+angNwat <- over(angling_hotspots_sp, NHD_WA_buff)
 # add lake ID onto angling events
-angling_hotspots_wat <- angling_hotspots
+angling_hotspots_wat <- angling_hotspots_sp@data
 angling_hotspots_wat$PermID <- angNwat$Permanent_
 angling_hotspots_wat$latitude_cent <- angNwat$latitude_cent
 angling_hotspots_wat$longitude_cent <- angNwat$longitude_cent
