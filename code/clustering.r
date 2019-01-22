39f42ad9dff51f6ac07f70e6b491dec59911d9ea
Spencer Wood <woodsp@stanford.edu>
2019-01-22 10:18:46 -0800

    add lake centroid coordinates to analysis table create a table of users (bobbers) by lake in wide format, with values for number of visits minor bug fixes

--- code/clustering.r
+++ code/clustering.r
@@ -1,5 +1,7 @@
+
 ###
-### Code for K-Means clustering of lakes
+### K-Means clustering of lakes
+###   run from top-level directory of angling repository
 ###
 
 # Read trip matrix
@@ -7,28 +9,33 @@ aud <- read.csv("./analysis/anglingXwater.csv", header=TRUE, na.strings=NULL, st
 
 # Reformat and delete missing values
 aud$date <- as.Date(aud$date)
-aud$Permanent_ <- as.numeric(aud$Permanent_)
+aud$PermID <- as.numeric(aud$PermID)
 aud_clean <- na.omit(aud)
+
+# Create table of users (rows) by lakes (cols)
+#  with number of visits as cell values
+library(reshape2)
+aud_clean_wide <- dcast(aud_clean, bobber_id ~ PermID, value.var="PermID", length)
+
 
 # Determine number of clusters using elbow method
 library(klaR)
 
-# Compute and plot within diff for k = 2 to k = 20.
+# Compute and plot within diff for k=2 to k=20.
 k.max <- 20
-wss <- sapply(1:k.max, 
-              function(k){set.seed(100000)
-                sum(kmodes(aud_clean, k, iter.max = 100 ,weighted = FALSE)$withindiff)})
-wss
+wss <- sapply(1:k.max, function(k) {
+                set.seed(100000)
+                sum(kmodes(aud_clean, k, iter.max=100, weighted=FALSE)$withindiff)
+              })
 
-plot(1:k.max, wss,
-     type="b", pch = 19, frame = FALSE, 
-     xlab="Number of clusters K",
-     ylab="Total within-clusters sum of squares")
+plot(1:k.max, wss, type="b", pch=19, frame=FALSE, xlab="Number of clusters K", ylab="Total within-clusters sum of squares")
 
 # K-Modes Cluster Analysis
-cluster.results <-kmodes(aud_clean, 6 ,iter.max = 100, weighted = FALSE ) 
+cluster.results <-kmodes(aud_clean, 6 ,iter.max=100, weighted=FALSE) 
 print(cluster.results)
 
 # Append cluster assignment
-kmclust <- factor(cluster.results$cluster)
-aud_clean$cluster <- kmclust
+aud_clean$cluster <- factor(cluster.results$cluster)
+
+
+
--- code/spatialjoin.r
+++ code/spatialjoin.r
@@ -1,5 +1,6 @@
 ###
 ### Code to intersect angling trips with lake polygons
+###   run this from the top level of the angling repository
 ###
 
 ## Inputs are angling events and polygons of lakes
@@ -25,23 +26,28 @@ angling_hotspots <- angling_hotspots[,c("hotspot_id", "latitude", "longitude", "
 # Subset 2017 records
 angling_hotspots <- angling_hotspots[which((angling_hotspots$date > "2016-12-31") & (angling_hotspots$date < "2018-01-01")), names(angling_hotspots) %in% c("hotspot_id", "latitude", "longitude", "bobber_id", "date", "hourMT")]
 
-# Subset USA records
+# Subset WA records
 angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 45 & angling_hotspots$latitude < 50 & angling_hotspots$longitude > -127 & angling_hotspots$longitude < -116, ]
 # angling_hotspots <- angling_hotspots[angling_hotspots$latitude > 25 & angling_hotspots$latitude < 50 & angling_hotspots$longitude > -127 & angling_hotspots$longitude < -65, ]
 
 ## Import/Munge Waterbodies
 # Read NHD spatial data
 library(rgdal)
+library(raster)
+library(rgeos)
 NHD_WA_raw <- readOGR(dsn="./data/", layer="NHDWaterbody_albers", stringsAsFactors=F)
 NHD_WA <- NHD_WA_raw
 
 # Filter NHD to lakes
 NHD_WA <- NHD_WA[which((NHD_WA@data$FCode >= "39000") & (NHD_WA@data$FCode <= "46599")), names(NHD_WA@data) %in% c("OBJECTID", "Permanent_", "FDate", "Resolution", "GNIS_ID", "GNIS_Name", "AreaSqKm", "Elevation", "ReachCode", "FType", "FCode", "Visibility", "Shape_Leng", "Shape_Area")]
 
+# Add lake centroid locations to NHD_WA
+NHD_WA_cent <- gCentroid(NHD_WA, byid=T)
+NHD_WA@data[ ,c("longitude_cent","latitude_cent")] <- coordinates(NHD_WA_cent)
+
 # Buffer NHD lakes by 50m
-library(raster)
-library(rgeos)
 NHD_WA_buff <- gBuffer(NHD_WA, width=50, byid=T)
+
 
 ## Intersect angling points with buffered lake polys
 #  start by reprojecting angling events to Albers to match waterbodies
@@ -52,7 +58,9 @@ angling_hotspots_sp_albers <- spTransform(angling_hotspots_sp, CRS("+proj=aea +l
 angNwat <- over(angling_hotspots_sp_albers, NHD_WA_buff)
 # add lake ID onto angling events
 angling_hotspots_wat <- angling_hotspots
-angling_hotspots_wat$OBJECTID <- angNwat$OBJECTID
+angling_hotspots_wat$PermID <- angNwat$Permanent_
+angling_hotspots_wat$latitude_cent <- angNwat$latitude_cent
+angling_hotspots_wat$longitude_cent <- angNwat$longitude_cent
 
 ## Cleanup and Export
 # Identified hotspots with duplicate bobber ID, lake, and date combinations to get trips
@@ -60,7 +68,7 @@ angling_hotspots_wat$bdoID <- paste(angling_hotspots_wat$bobber_id, angling_hots
 angling_hotspots_wat$uniq <- !duplicated(angling_hotspots_wat$bdoID)
 
 # Table for first round of analyses
-toa <- angling_hotspots_wat[angling_hotspots_wat$uniq==T,c("bobber_id","date","OBJECTID")]
+toa <- angling_hotspots_wat[angling_hotspots_wat$uniq==T,c("bobber_id","date","PermID","latitude_cent","longitude_cent")]
 
 # Write table to analyse
 write.csv(toa, "./analysis/anglingXwater.csv", row.names=F)
